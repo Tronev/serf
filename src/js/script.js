2f41dc3bc6ffd35819a02ab21424c283fe10cab1
Tronev_Ivan <74rus_86@mail.ru>
2020-03-31 15:53:05 +0600

    хедер готов. необходимо реализовать начало анимации при переключении слайдера

--- src/js/script.js
+++ src/js/script.js
@@ -5,14 +5,7 @@ $(function(){
         fade:true,
         prevArrow: '<img class="slider-arrows slider-arrows-left" src="img/arrows-left.svg" alt="arrows-right">',
         nextArrow: '<img class="slider-arrows slider-arrows-right" src="img/arrows-right.svg" alt="arrows-right">',
-        // asNavFor: '.slider-dots',
     });
-
-    // $('.slider-dots').slick({
-    //     slidesToShow: 4,
-    //     slidesToScroll: 4,
-    //     // asNavFor: '.header__slider',
-    // });
 
     $('.slider-dots')
         .on('init', function(event, slick) {
@@ -37,5 +30,5 @@ $(function(){
         $('.header__slider').slick('slickGoTo', goToSingleSlide);
     });
 
-    // https://codepen.io/dolce/pen/QarQde 
+    // https://codepen.io/dolce/pen/QarQde  сайт откуда взяты настройки слайдера
 });
