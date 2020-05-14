2e4418b21915b4830360fd97428df057914251f6
Tronev_Ivan <74rus_86@mail.ru>
2020-05-14 08:05:41 +0600

    section surf

--- src/js/script.js
+++ src/js/script.js
@@ -29,6 +29,21 @@ $(function(){
    
         $('.header__slider').slick('slickGoTo', goToSingleSlide);
     });
-
     // https://codepen.io/dolce/pen/QarQde  сайт откуда взяты настройки слайдера
+
+    $(".surf-slider").slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        
+        prevArrow: '<img class="slider-arrows slider-arrows-left" src="img/arrows-left.svg" alt="arrows-right">',
+        nextArrow: '<img class="slider-arrows slider-arrows-right" src="img/arrows-right.svg" alt="arrows-right">',
+        asNavFor: ".surf-map",
+    });
+
+    $(".surf-map").slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        asNavFor: ".surf-slider",
+        focusOnSelect: true,
+    });
 });
