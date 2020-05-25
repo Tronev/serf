24c93f5746cef556c0e5872d5a334cc4f465ee27
Tronev_Ivan <74rus_86@mail.ru>
2020-05-25 06:20:49 +0600

    sleep section

--- src/js/script.js
+++ src/js/script.js
@@ -46,4 +46,13 @@ $(function(){
         asNavFor: ".surf-slider",
         focusOnSelect: true,
     });
+
+    $(".travel__slider").slick({
+        infinite:true,
+        fade:true,
+        
+        prevArrow: '<img class="slider-arrows slider-arrows-left" src="img/arrows-left.svg" alt="arrows-right">',
+        nextArrow: '<img class="slider-arrows slider-arrows-right" src="img/arrows-right.svg" alt="arrows-right">',
+        
+    });
 });
