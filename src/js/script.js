4aa20d9a4750ba2308902868184d7efb62e07089
Tronev_Ivan <74rus_86@mail.ru>
2020-06-07 17:37:19 +0600

    section sleep

--- src/js/script.js
+++ src/js/script.js
@@ -55,4 +55,57 @@ $(function(){
         nextArrow: '<img class="slider-arrows slider-arrows-right" src="img/arrows-right.svg" alt="arrows-right">',
         
     });
+
+    $(".sleep__slider").slick({
+        infinite:true,
+        fade:true,
+        
+        prevArrow: '<img class="slider-arrows slider-arrows-left" src="img/arrows-left.svg" alt="arrows-right">',
+        nextArrow: '<img class="slider-arrows slider-arrows-right" src="img/arrows-right.svg" alt="arrows-right">',
+        
+    });
+
+    // плюсики минусики на sleep
+    $('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="img/plus.svg" alt="plus"></div><div class="quantity-button quantity-down"><img src="img/minus.svg" alt="minus"></div></div>').insertAfter('.quantity input');
+    $('.quantity').each(function() {
+      var spinner = $(this),
+        input = spinner.find('input[type="number"]'),
+        btnUp = spinner.find('.quantity-up'),
+        btnDown = spinner.find('.quantity-down'),
+        min = input.attr('min'),
+        max = input.attr('max');
+
+      btnUp.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue >= max) {
+          var newVal = oldValue;
+        } else {
+          var newVar = oldValue + 1;
+        }
+        spinner.find("input").val('newVar');
+        spinner.find("input").trigger("change");
+      });
+
+      btnDown.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue <= min) {
+          var newVall = oldValue;
+        } else {
+          var newVar = oldValue - 1;
+        }
+        spinner.find("input").val('newVar');
+        spinner.find("input").trigger("change");
+      });
+    });
+
+     // калькулятор для расчета стоимости  ночи и ко-во гостей
+
+    $('.quantity-button').on('click', function(){
+      var summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
+     $('.summ').html('$' + summ);
+
+    });
+
+    var summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
+     $('.summ').html('$' + summ);
 });
